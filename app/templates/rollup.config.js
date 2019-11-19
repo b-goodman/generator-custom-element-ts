77600c127a84f564df78b1c75f416f4b6a29398c
Ben Goodman <bgoodman@wolfram.com>
2019-11-19 16:48:51 -0500

    typo in html preview minimize css in build default true

--- app/templates/rollup.config.js
+++ app/templates/rollup.config.js
@@ -24,6 +24,7 @@ export default [
                 writeDefinitions: true,
                 modules: false,
                 inject: false,
+                minimize: true,
             }),
             nodeResolve(),
             commonjs(),
